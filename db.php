4a1b6caad476223cc0edfa398190ca4db8c9363c
David Morales <50034808+dmorales22@users.noreply.github.com>
2021-11-07 22:45:40 -0700

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
-<?php //Connects to UTEP's DB server
-$host = 'cssrvlab01.utep.edu';// Change as necessary
-$data = 'dmorales22_f21_db'; // Change as necessary
-$user = 'dmorales22'; // Change as necessary
-$pass = '*utep2021!'; // Change as necessary
+<?php //Connects to DB server
+$host = '';// Change as necessary
+$data = ''; // Change as necessary
+$user = ''; // Change as necessary
+$pass = ''; // Change as necessary
 $chrs = 'utf8mb4';
 $attr = "mysql:host=$host;dbname=$data;charset=$chrs";
 
